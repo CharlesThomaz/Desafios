bdd456fa79bf455334d666b24b65d7223db857e1
Charles Thomaz <138392103+CharlesThomaz@users.noreply.github.com>
2024-06-14 11:49:48 -0300

    iPhone_Final
    Projeto Finalizado com as classes implementadas

---
+++ DesafioIphone/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        iPhone meuIphone = new iPhone();
+
+        // Testando funcionalidades de Reprodutor Musical
+        meuIphone.selecionarMusica("Imagine - John Lennon");
+        meuIphone.tocar();
+        meuIphone.pausar();
+
+        // Testando funcionalidades de Aparelho Telefônico
+        meuIphone.ligar("123-456-7890");
+        meuIphone.atender();
+        meuIphone.iniciarCorreioVoz();
+
+        // Testando funcionalidades de Navegador na Internet
+        meuIphone.exibirPagina("https://www.example.com");
+        meuIphone.adicionarNovaAba();
+        meuIphone.atualizarPagina();
+    }
+}
